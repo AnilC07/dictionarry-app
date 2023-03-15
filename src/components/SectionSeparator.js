b3e01bcb8e3d76722403968b58b302084d2bdd06
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-03-15 11:06:51 +0100

    fix warning

--- src/components/SectionSeparator.js
+++ src/components/SectionSeparator.js
@@ -7,7 +7,7 @@ import classes from './SectionSeparator.module.css'
 const SectionSeparator = (props) => {
   return (
     <div className={classes.separator}>
-    <h4></h4> <span className={classes.trait}></span>
+    <h4>name</h4> <span className={classes.trait}></span>
     </div>
   )
 }
