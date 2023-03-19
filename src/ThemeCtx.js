78492430cc4fb9e7e9a9aef0bf7cc38e76b173d3
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-03-19 18:44:21 +0100

    remove files

--- src/ThemeCtx.js
+++ src/ThemeCtx.js
@@ -39,3 +39,40 @@ export const ThemeContext = createContext({
 
 
 export default ThemeCtx
+
+
+const express = require('express');
+const fs = require('fs');
+
+// Install it via npm : 'npm i morgan'
+const morgan = require('morgan');
+
+const app = express();
+
+// Like body-parser
+// Middleware show requests informations
+app.use(morgan('dev'))
+// Middleware body-parser
+app.use(express.json());
+
+// Example of middleware
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 🐣');
+  next();
+});
+
+// 3) ROUTES
+
+app.route('/api/v1/tours')
+  .get(handler)
+  .post(handler);
+app.route('/api/v1/tours/:id')
+  .get(handler)
+  .patch(handler)
+  .delete(handler);
+
+// Run server
+const port = '3000';
+app.listen(port, () => {
+  console.log(`Server listenin on port port...`);
+});
